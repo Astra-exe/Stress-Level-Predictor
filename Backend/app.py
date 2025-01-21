9716bf1dc29824bedefa90eb610dc81e1c090f7f
Astra-exe <juan.ramirez.j99@gmail.com>
2025-01-21 16:06:21 -0600

    [feat] Enable CORS and update predict endpoint to POST with input data validation

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_limiter import Limiter
+from flask_cors import CORS
 from flask_limiter.util import get_remote_address
 import joblib
 import pandas as pd
@@ -10,6 +11,9 @@ model = joblib.load('stress_predict.pkl')
 #Create a Flask app
 app = Flask(__name__)
 
+#Allow Cross-Origin requests
+CORS(app)
+
 #Rate limit the API
 limiter = Limiter(
     get_remote_address,
@@ -19,7 +23,7 @@ limiter = Limiter(
 
 
 #Create an API endpoint
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 @limiter.limit("10 per minute")
 def predict():
     try:
@@ -28,6 +32,9 @@ def predict():
 
         #Convert the data into a pandas DataFrame
         input_data = pd.DataFrame([input_data])
+
+        #make sure that the data is in the correct order
+        input_data = input_data[['Age', 'Sleep Duration', 'Heart Rate', 'Daily Steps', 'Occupation_encoded', 'BMI_category_encoded','Gender_encoded']]
 
         #Make a prediction
         prediction = model.predict(input_data)
@@ -38,6 +45,7 @@ def predict():
         # return '<h1>The stress level is: ' + str(int(prediction[0])) + '</h1>'
     
     except Exception as e:
+        print(str(e))
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
