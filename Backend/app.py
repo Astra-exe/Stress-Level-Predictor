2140924b6404887e527d9b003095fd90e14b660c
Astra-exe <juan.ramirez.j99@gmail.com>
2025-01-27 13:53:46 -0600

    [feat] Add BMI calculation and encoding to the prediction input data

--- Backend/app.py
+++ Backend/app.py
@@ -5,6 +5,15 @@ from flask_limiter.util import get_remote_address
 import joblib
 import pandas as pd
 
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    if bmi < 25:
+        return 0 #normal
+    elif 25 <= bmi < 30:
+        return 1 #overweight
+    else:
+        return 2 #obese
+    
 #Load the ML model
 model = joblib.load('stress_predict.pkl')
 
@@ -32,6 +41,12 @@ def predict():
 
         #Convert the data into a pandas DataFrame
         input_data = pd.DataFrame([input_data])
+
+        # Calculate the BMI category
+        input_data['BMI_category_encoded'] = input_data.apply(lambda x: calculate_bmi(x['Weight'], x['Height']), axis=1)
+
+        #drop the weight and height columns
+        input_data = input_data.drop(['Weight', 'Height'], axis=1)
 
         #make sure that the data is in the correct order
         input_data = input_data[['Age', 'Sleep Duration', 'Heart Rate', 'Daily Steps', 'Occupation_encoded', 'BMI_category_encoded','Gender_encoded']]
