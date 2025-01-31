003d48b2e215db8f378db9932925ce46d47cda36
francisco-solis99 <franciscosoliscss@gmail.com>
2025-01-31 14:34:38 -0600

    [fix] into API request data formated

--- Frontend/src/services/stress.ts
+++ Frontend/src/services/stress.ts
@@ -1,12 +1,22 @@
 import { StressFormData } from "../types"
 
 export async function getStressData(userData: StressFormData) {
+  const requestDataFormated = {
+    Age: userData.age,
+    Gender_encoded: userData.gender,
+    Height: userData.height,
+    Weight: userData.weight,
+    Occupation_encoded: userData.job,
+    "Sleep Duration": userData.sleepHours,
+    "Daily Steps": userData.stepsByDay,
+    "Heart Rate": userData.heartRatio,
+  }
   const data = await fetch('http://127.0.0.1:3000/predict', {
     headers: {
       "Content-Type": "application/json",
     },
     method: 'POST',
-    body: JSON.stringify(userData)
+    body: JSON.stringify(requestDataFormated)
   })
   const jsonData = await data.json()
   return jsonData
