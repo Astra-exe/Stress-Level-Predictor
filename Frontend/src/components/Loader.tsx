4cb1dfbc42a1603f995ac11708f23f6d5600affc
francisco-solis99 <franciscosoliscss@gmail.com>
2025-02-11 14:30:58 -0600

    [feat] add loader component

--- Frontend/src/components/Loader.tsx
+++ Frontend/src/components/Loader.tsx
@@ -1,3 +1,20 @@
-export default function Loader() {
-  return <div>Loader</div>;
+import { useEffect } from "react";
+
+export default function Loader({ nextStatus }) {
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("loading");
+      nextStatus("results");
+    }, 2000);
+  }, [nextStatus]);
+
+  return (
+    <div className="relative min-h-[90vh]">
+      <div className="flex flex-row gap-2 absolute top-1/2 left-1/2">
+        <div className="w-4 h-4 rounded-full bg-red-500 animate-bounce"></div>
+        <div className="w-4 h-4 rounded-full bg-red-500 animate-bounce [animation-delay:-.3s]"></div>
+        <div className="w-4 h-4 rounded-full bg-red-500 animate-bounce [animation-delay:-.5s]"></div>
+      </div>
+    </div>
+  );
 }
