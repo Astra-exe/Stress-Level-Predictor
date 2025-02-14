6fb2271ed9882958a8f1ce4dcfe470ea564db883
francisco-solis99 <franciscosoliscss@gmail.com>
2025-02-14 12:58:13 -0600

    [style] fix colro loader component

--- Frontend/src/components/Loader.tsx
+++ Frontend/src/components/Loader.tsx
@@ -5,15 +5,15 @@ export default function Loader({ nextStatus }) {
     setTimeout(() => {
       console.log("loading");
       nextStatus("results");
-    }, 2000);
+    }, 5000);
   }, [nextStatus]);
 
   return (
     <div className="relative min-h-[90vh]">
       <div className="flex flex-row gap-2 absolute top-1/2 left-1/2">
-        <div className="w-4 h-4 rounded-full bg-red-500 animate-bounce"></div>
-        <div className="w-4 h-4 rounded-full bg-red-500 animate-bounce [animation-delay:-.3s]"></div>
-        <div className="w-4 h-4 rounded-full bg-red-500 animate-bounce [animation-delay:-.5s]"></div>
+        <div className="w-4 h-4 rounded-full bg-primary animate-bounce"></div>
+        <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:-.3s]"></div>
+        <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:-.5s]"></div>
       </div>
     </div>
   );
